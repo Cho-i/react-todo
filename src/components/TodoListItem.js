612c4b3018f2ab710191704b2232ddfd09ac1f63
Lee cho i <cho.i.lee.1223@gmail.com>
2022-08-22 18:31:20 +0900

    todo

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,13 +1,11 @@
-import { useState } from 'react';
 import React from 'react';
 
 function TodoListItem(props) {
-
   return (
     <li>
-      <input type="checkbox" id={props.todo.id} onChange={(e)=>{
+      <input type="checkbox" checked={props.todo.checked} id={props.todo.id} onChange={(e)=>{
         const copy = [...props.todos]
-        copy[props.i].checked = !props.todo.checked
+        copy[props.i].checked = e.target.checked
         props.setTodos(copy)
       }}/>
       <label htmlFor={props.todo.id}>{props.todo.text}</label>
@@ -15,8 +13,8 @@ function TodoListItem(props) {
       <button onClick={(e)=>{
         const copy = [...props.todos]
         copy.splice(props.i,1)
-        props.setTodos(copy)
-      }}>삭제</button>
+        props.setTodos(copy)     
+      }}>삭제</button>  
     </li>
   );
 }
